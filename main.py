77f6c6a5020d8f8ccc010e108596de11ad95cf70
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-06-23 22:33:14 -0600

    the settings have settings now

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import pygame
 import datetime
 import os
+import json
+
+
 pygame.init()
+
+f = open("settings.json", "r")
+settings = f.read()
+f.close
+
 
 
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
@@ -9,7 +17,9 @@ pygame.display.set_caption("clock")
 screen_size = pygame.display.get_window_size()
 screen_width = screen_size[0]
 screen_height = screen_size[1]
-font = pygame.font.SysFont("sans", int(screen_width / 5), bold=True)
+font = "sans"
+
+
 print(screen_size)
 class button:
     def __init__(self,x,y,width,height,image_path,bg_color = None):
@@ -37,6 +47,7 @@ class menu:
         menu_y = screen_height/20
         menu_width = screen_width - screen_width/10
         menu_height = screen_height - screen_height/10
+        
         self.bg = pygame.Rect(
             menu_x,
             menu_y,
@@ -49,16 +60,36 @@ class menu:
             menu_width + screen_width/30,
             menu_height + screen_width/30
         )
+    def draw_text(self, text, number, text_size):
+        menu_font = pygame.font.SysFont(font, text_size, True)
+        text_surface = menu_font.render(text, True, (0, 0, 0))
+        text_rect = text_surface.get_rect()
+        text_rect.topleft = (self.bg.x + self.bg.width / 100, self.bg.y + self.bg.width/100 + number * text_rect.height * 1.2)
+        pygame.draw.rect(screen,(255,255,255), text_rect)
+        screen.blit(text_surface, text_rect) 
         
+        return text_rect
     def open(self):
         self.enabled = True
     def close(self):
         self.enabled = False
     def draw(self):
         if self.enabled == True:
-            
+            text_size = int(screen_width / 20)
             pygame.draw.rect(screen,(255,255,255), self.outline)
             pygame.draw.rect(screen,self.color,self.bg)
+            self.draw_text(self.name, 0, text_size)
+            
+            self.draw_text("font = " + font, 1, text_size)
+            
+            self.draw_text("save", 6, text_size)
+            
+            
+            
+            
+            
+            
+            
             
             
     
@@ -73,7 +104,7 @@ settings = button(
     screen_width/10,
     "settings.png"
 )
-# Font setup
+
 running = True
 while running:
     
@@ -82,8 +113,8 @@ while running:
     now = datetime.datetime.now()
     time = now.strftime("%I:%M %p")
 
-
-    text_surface = font.render(time, True, (255, 255, 255))
+    time_font = pygame.font.SysFont("sans", int(screen_width / 5), bold=True)
+    text_surface = time_font.render(time, True, (255, 255, 255))
     text_rect = text_surface.get_rect(center=(screen_width / 2, screen_height / 2))
     
 
@@ -106,9 +137,8 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if settings.get_rect().collidepoint(event.pos):
-                if settings_menu.enabled:
-                    settings_menu.close()
-                else:
+                
+                if settings_menu.enabled == False:
                     settings_menu.open()
 
     pygame.time.delay(1)
